93d41686954387a17cb117d2d501fbf82fe2b725
Samriddhi Singh <samsris06@gmail.com>
2026-07-07 13:50:57 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector <pair<int, int>> time;
+        for(auto i: intervals){
+            time.push_back({i.start, 1});
+            time.push_back({i.end, -1});
+        }
+        sort(time.begin(), time.end(),[](auto const &a, auto const &b){
+            return a.first==b.first?a.second<b.second:a.first<b.first;
+        });
+    // The sorting algorithm takes care of non-conflict operations
+        int res = 0;
+        int count = 0;
+        for(auto t : time){
+            count += t.second;
+            res = max(res, count);
+        }
+
+        return res;
+    }
+};
