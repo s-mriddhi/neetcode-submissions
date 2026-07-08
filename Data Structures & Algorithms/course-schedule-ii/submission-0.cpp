f544486f44bf39720d08c3ba31dd5603dfdb048f
Samriddhi Singh <samsris06@gmail.com>
2026-07-08 13:38:27 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+         vector <vector<int>> adjList(n);
+        for(auto p : prerequisites){
+            adjList[p[1]].push_back(p[0]);
+        }
+        vector <int> indegree(n,0);
+        for(auto node: adjList){
+            for(auto adjNode : node){
+                indegree[adjNode]++;
+            }
+        }
+        vector <int> result;
+        queue <int> q;
+        for(int i = 0; i<n ; i++){
+            if(indegree[i]==0){
+                q.push(i);
+                result.push_back(i); // this covers even disconnected components
+            }
+        }
+
+        while(!q.empty()){
+            auto top = q.front();
+            q.pop();
+            for(auto adj : adjList[top]){
+                indegree[adj]--;
+                if(!indegree[adj]){
+                    result.push_back(adj);
+                    q.push(adj);
+                }
+            }
+        }
+        
+        if(result.size()==n) return result;
+        return {};
+    }
+    
+};
