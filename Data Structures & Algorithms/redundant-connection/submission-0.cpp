2834d08f6fab1d9b1b81e5ade21f04f6bb43576f
Samriddhi Singh <samsris06@gmail.com>
2026-07-08 15:16:42 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,53 @@
+class DSU {
+    vector <int> parent, size;
+    int comps;
+public:
+    DSU(int n){
+        comps = n;
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        for(int i = 0; i<n+1; i++){
+            parent[i]=i;
+        }
+    }
+
+    int findParent(int node){
+        if(parent[node]!=node){
+            parent[node] = findParent(parent[node]);
+        }
+        return parent[node];
+    }
+
+    bool unionBySize(int u, int v){
+        int par_u = findParent(u);
+        int par_v = findParent(v);
+        if(par_u==par_v) return false;
+        if(size[par_u]>size[par_v]){
+            parent[par_v] = par_u;
+            size[par_u]+=size[par_v];
+        }
+        else{
+            parent[par_u] = par_v;
+            size[par_v]+=size[par_u];
+        }
+        comps--;
+        return true;
+    }
+
+    int components(){
+        return comps;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        DSU d(n);
+        for(auto edge : edges){
+            if(d.unionBySize(edge[0], edge[1])==false) return edge;
+        }
+        return {-1,-1};
+    }
+};
